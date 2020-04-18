fcfd015cdbd35d8e67fbad4aa16963ede9fafce4
Ahmed-Elharty <ahmed.elharty6@gmail.com>
2020-04-18 14:05:46 +0200

    menu navbar

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import {} from '@fortawesome/free-brands-svg-icons';
+import {} from '@fortawesome/free-regular-svg-icons';
+import {faDrumstickBite,faCircle,faWineGlass,faPizzaSlice,faFish,faUtensils,faCheese} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-
+Chickenicon=faDrumstickBite;
+allicon=faCircle;
+drinkicon=faWineGlass;
+pizzaicon=faPizzaSlice;
+seafoodicon=faFish;
+meaticon=faUtensils;
+appetizersicon=faCheese;
   constructor() { }
 
   ngOnInit(): void {
