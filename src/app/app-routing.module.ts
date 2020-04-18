cf4394c87485a2a557ab53ebd98c0462798e77ba
Ahmed-Elharty <ahmed.elharty6@gmail.com>
2020-04-18 11:13:11 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MenuComponent} from './menu/menu.component'
+
+const routes: Routes = [
+{ 
+  path:'', component:MenuComponent
+
+}
 
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
