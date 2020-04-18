00e013565fd24bfd9475f424994df47dba86271f
Ahmed-Elharty <ahmed.elharty6@gmail.com>
2020-04-18 15:58:53 +0200

    menu card

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {} from '@fortawesome/free-brands-svg-icons';
 import {} from '@fortawesome/free-regular-svg-icons';
-import {faDrumstickBite,faCircle,faWineGlass,faPizzaSlice,faFish,faUtensils,faCheese} from '@fortawesome/free-solid-svg-icons';
+import {faDrumstickBite,faCircle,faWineGlass,faPizzaSlice,faFish,faUtensils,faCheese,faPlusCircle,faMinusCircle,faCartPlus,faCommentAlt} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -15,6 +15,10 @@ pizzaicon=faPizzaSlice;
 seafoodicon=faFish;
 meaticon=faUtensils;
 appetizersicon=faCheese;
+plusicon=faPlusCircle;
+minusicon=faMinusCircle;
+addtocart=faCartPlus;
+addComment=faCommentAlt;
   constructor() { }
 
   ngOnInit(): void {
